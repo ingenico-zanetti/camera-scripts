9a634a96ef394884fb497501d70f4239ca59cb65
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-03-03 17:20:26 +0100

    Use actual 4K data from the sensor instead of 2x2 binning. These provides much better quality but requires kernel patch to overclock the RP1 from 200MHz to 300MHz.

--- imx283/h264stream.bash
+++ imx283/h264stream.bash
@@ -1,18 +1,16 @@
 #!/bin/bash
-. ~/bin/roi.bash
-if true
-then
-while sleep 1 ; do
-	/usr/local/bin/libcamera-vid --exposure sport --metering average --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" --mode 2784:1828:12 ${roi}  --bitrate 48000000 --gain 4 --framerate 30 -t 0 --width 1920 --height 1080 --codec h264 -o - --libav-format h264 2>>2K.err  | ./h264streamer | tee $(date +h264streamer_%Y%m%d-%H%M%S.log)  
+gain=" --gain 1 "
+# perfectly centered roi wouldbe roi=" --roi 0.1491,0.1491,0.7018,0.7018 "
+# but the sensor is *not* exactly centered on the optical axis
+mode=" --mode 5568:3094:10 "
+roi=" --roi 0.1422,0.2500,0.7018,0.7018 "
+# resolution=" --width 3840 --height 2160 "
+# fps=" --framerate 3 "
+resolution=" --width 1920 --height 1080 "
+fps=" --framerate 30 "
+bitrate=" --bitrate 18000000 "
+while true ; do
+	/usr/local/bin/libcamera-vid --exposure sport --metering centre --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" ${mode} ${roi} ${bitrate} ${gain} ${framerate}  -t 0 ${resolution} --codec h264 -o - --libav-format h264 2>>./h264.err | ./h264streamer | tee $(date +h264streamer_%Y%m%d-%H%M%S.log)  
+	sleep 1
 done
-else
-	while sleep 1 
-	do
-		/usr/local/bin/libcamera-vid --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
-			--mode 5568:3664:12 --roi 0.1552,0.2046,0.6896,0.5908 \
-			--bitrate 48000000 --gain 1 --framerate 15 -t 0 --width 3840 --height 2160 \
-			--codec h264 --quality 85 --metering spot \
-			--listen -o tcp://0.0.0.0:56789/ 2>&1 | tee $(date +%Y%m%d-%H%M%S.txt) 
-	done
-fi
 
