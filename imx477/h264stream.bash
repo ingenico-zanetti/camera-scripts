0400f15d187934b5f04e02454fd645457a64bb48
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-03-03 02:38:34 +0100

    Thanks to a patched kernel by 6by9, imx477 2-lanes can now output 4056x2160x12 at more than 30fps. So use it ! UHD=1 => 4K at 10 fps (not sure encoding can handle) UHD=0 => 4K capture, but 1080@30fps output UHD not defined => 2K capture, 1080p@30fps output

--- imx477/h264stream.bash
+++ imx477/h264stream.bash
@@ -1,27 +1,17 @@
 #!/bin/bash
-. ./roi.bash
 gain=" --gain 4 "
-shutter=" --shutter 32000 "
 
-if [ "${UHD}" != "" ]
+if [ "${UHD}" == "1" ]
 then
 	echo "4K UHD"
 	mode=" --mode 4056:3040:12 "
 	width=" --width 3840 "
 	height=" --height 2160 "
-	fps=" --framerate 10 "
-else
-	echo "FHD 1080p"
-	mode=" --mode 2028:1080:12 "
 	roi=" --roi 0.045,0,0.9468,1 "
-	width=" --width 1920 "
-	height=" --height 1080 "
-	fps=" --framerate 30 "
-fi
-
+	fps=" --framerate 10 "
 while sleep 1
 do
-	libcamera-vid --verbose \
+	/usr/local/bin/libcamera-vid --verbose \
 		--info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
 		${mode} \
 		${roi} \
@@ -34,6 +24,58 @@ do
 		${height} \
 		--codec h264 \
 		--rotation 180 \
-		--libav-format h264 -o - 2>/dev/null | ./h264streamer
+		--libav-format h264 -o -  2>>./h264.log | ./h264streamer
 done
+elif [ "${UHD}" == "0" ]
+then
+	# thanks to a kernel patch by 6by9 we have 4056:2160 12bpp at 32.39fps
+	echo "4K -> 1080p"
+	mode=" --mode 4056:2160:12 "
+	width=" --width 1920 "
+	height=" --height 1080 "
+	fps=" --framerate 30 "
+	roi=" --roi=0.0450,0.1447,0.9468,1 "
+while sleep 1
+do
+	/usr/local/bin/libcamera-vid --verbose \
+		--info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
+		${mode} \
+		${roi} \
+		--bitrate 18000000 \
+		${gain} \
+		${shutter} \
+		${fps} \
+		-t 0 \
+		${width} \
+		${height} \
+		--codec h264 \
+		--rotation 180 \
+		--libav-format h264 -o -  2>>./h264.log | ./h264streamer
+done
+else
+	echo "FHD 1080p"
+	mode=" --mode 2028:1080:12 "
+	roi=" --roi 0.045,0,0.9468,1 "
+	width=" --width 1920 "
+	height=" --height 1080 "
+	fps=" --framerate 30 "
+while sleep 1
+do
+	/usr/local/bin/libcamera-vid --verbose \
+		--info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
+		${mode} \
+		${roi} \
+		--bitrate 18000000 \
+		${gain} \
+		${shutter} \
+		${fps} \
+		-t 0 \
+		${width} \
+		${height} \
+		--codec h264 \
+		--rotation 180 \
+		--libav-format h264 -o -  2>>./h264.log | ./h264streamer
+done
+fi
+
 
