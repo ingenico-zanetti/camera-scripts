5806f2330325cf6224efc4b337a07af32d4847ce
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-03-05 02:33:35 +0100

    Gère les modes 4K, 4K vers 1080p et binning 2x2 vers 1080p

--- imx283/h264stream.bash
+++ imx283/h264stream.bash
@@ -1,16 +1,47 @@
 #!/bin/bash
-gain=" --gain 1 "
-# perfectly centered roi wouldbe roi=" --roi 0.1491,0.1491,0.7018,0.7018 "
-# but the sensor is *not* exactly centered on the optical axis
-mode=" --mode 5568:3094:10 "
-roi=" --roi 0.1422,0.2500,0.7018,0.7018 "
-# resolution=" --width 3840 --height 2160 "
-# fps=" --framerate 3 "
-resolution=" --width 1920 --height 1080 "
-fps=" --framerate 30 "
+if [ "$UHD" == "0" ]
+then
+	mode=" --mode 5568:3094:10 "
+	# perfectly centered roi would be roi=" --roi 0.1491,0.1491,0.7018,0.7018 "
+	# but the sensor is *not* exactly centered on the optical axis
+	#
+	# x=0.1382 => crop (main) (756, yyy) on est au plus proche de la perfection
+	#
+	# y=0.2560 => crop (main) (xxx,1072) on est au plus proche de la perfection
+	#
+	roi=" --roi 0.1382,0.2560,0.7018,0.7018 "
+	resolution=" --width 1920 --height 1080 "
+	gain=" --gain 4 "
+	logfile="UHD=0.log"
+	fps=" --framerate 30 "
+elif [ "$UHD" == "1" ]
+then
+	mode=" --mode 5568:3094:10 "
+	roi=" --roi 0.1382,0.2560,0.7018,0.7018 "
+	resolution=" --width 3840 --height 2160 "
+	gain=" --gain 4 "
+	logfile="UHD=1.log"
+	fps=" --framerate 3 "
+else
+	mode=" --mode 2784:1828:12 "
+	# perfectly centered roi would be roi=" --roi 0.1491,0.1491,0.7018,0.7018 "
+	# but the sensor is *not* exactly centered on the optical axis
+	#
+	# y=0.2166 => crop (main) (xxx, 790) on est au plus proche de la perfection
+	#
+	# x=0.1382 => crop (main) (756, yyy) on est au plus proche de la perfection
+	#
+	roi=" --roi 0.1382,0.2166,0.7018,0.5923 "
+	resolution=" --width 1920 --height 1080 "
+	gain=" --gain 4 "
+	logfile="2K.log"
+	fps=" --framerate 30 "
+fi
+shutter=" --shutter 16000 "
 bitrate=" --bitrate 18000000 "
-while true ; do
-	/usr/local/bin/libcamera-vid --exposure sport --metering centre --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" ${mode} ${roi} ${bitrate} ${gain} ${framerate}  -t 0 ${resolution} --codec h264 -o - --libav-format h264 2>>./h264.err | ./h264streamer | tee $(date +h264streamer_%Y%m%d-%H%M%S.log)  
+
+#/ while true ; do
+	/usr/local/bin/libcamera-vid ${shutter} --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" ${mode} ${roi} ${bitrate} ${gain} ${fps}  -t 0 ${resolution} --codec h264 -o - --libav-format h264 2>>${logfile} | ./h264streamer 
 	sleep 1
-done
+#/ done
 
