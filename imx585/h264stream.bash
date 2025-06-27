e7c941117bf3c6c06b8d502c4544abf5263c0077
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-06-27 09:43:08 +0200

    Allow quality to be specified through Q 2x2 binned is used for 60fps

--- imx585/h264stream.bash
+++ imx585/h264stream.bash
@@ -7,6 +7,7 @@ then
 	gain=" --gain 1 "
 	logfile="UHD=0.log"
 	fps=" --framerate 30 "
+	bitrate=" --bitrate 48000000 "
 elif [ "$UHD" == "1" ]
 then
 	mode=" --mode 3856:2180:12 "
@@ -14,18 +15,19 @@ then
 	resolution=" --width 3840 --height 2160 "
 	gain=" --gain 1 "
 	logfile="UHD=1.log"
-	fps=" --framerate 3 "
+	fps=" --framerate 15 "
+	bitrate=" --bitrate 48000000 "
 else
 	mode=" --mode 1928:1090:12 "
 	roi=" --roi 0,0,1,1 "
 	resolution=" --width 1920 --height 1080 "
-	gain=" --gain 1 "
+	gain=" --gain 4 "
 	logfile="2K.log"
-	fps=" --framerate 30 "
+	fps=" --framerate 60 "
+	bitrate=" --bitrate 48000000 "
 fi
 # shutter=" --shutter 4000 "
 # wb="  --awbgains 1.56,2.15 "
-bitrate=" --bitrate 18000000 "
 # preview=" --nopreview "
 while true ; do
 	/usr/local/bin/libcamera-vid ${preview} ${shutter} ${wb} --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" ${mode} ${roi} ${bitrate} ${gain} ${fps}  -t 0 ${resolution} --codec h264 -o - --libav-format h264 2>>${logfile} | ./h264streamer 
--- imx585/mjpegstream.bash
+++ imx585/mjpegstream.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ "" = "$Q" ]
+then
+	quality=" --quality 75 "
+else
+	quality=" --quality ${Q} "
+fi
+
 if [ "" = "$UHD" ]
 then
 	while sleep 1 
@@ -6,8 +14,8 @@ then
 		echo 1080pbin2x2
 		libcamera-vid --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
 			--mode 1928:1090:12  \
-			--gain 1 --framerate 30 -t 0 --width 1920 --height 1080 \
-			--codec mjpeg --quality 75 --metering average --exposure sport \
+			--gain 1 --framerate 60 -t 0 --width 1920 --height 1080 \
+			--codec mjpeg ${quality} --metering average --exposure sport \
 		       	-o - --libav-format mjpeg  2>>2x2.err | ./mjpegstreamer
 	done
 elif [ 1 -eq "$UHD" ]
@@ -18,7 +26,7 @@ then
 		libcamera-vid --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
 			--mode 3856:2180:12  \
 			--gain 1 --framerate 30 -t 0 --width 3840 --height 2160 \
-			--codec mjpeg --quality 75 --metering average --exposure sport \
+			--codec mjpeg ${quality} --metering average --exposure sport \
 			-o - --libav-format mjpeg 2>>4K.err | ./mjpegstreamer
 	done
 else
@@ -28,7 +36,7 @@ else
 		libcamera-vid --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
 			--mode 3856:2180:12 \
 			--gain 1 --framerate 30 -t 0 --width 1920 --height 1080 \
-			--codec mjpeg --quality 75 --metering average --exposure sport \
+			--codec mjpeg ${quality} --metering average --exposure sport \
 		       	-o - --libav-format mjpeg 2>>2K.err | ./mjpegstreamer
 	done
 fi
