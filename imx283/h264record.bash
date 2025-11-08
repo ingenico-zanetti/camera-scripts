fd1bff0d510400494ee543fd4f48e86ed374a36c
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-11-08 16:11:16 +0100

    Shutdown sequence is not compatible with mp4 now, because ffmpeg doesn't get any warning we are shutting down. Revert to h264 stream recording until a working solution is found.

--- imx283/h264record.bash
+++ imx283/h264record.bash
@@ -4,13 +4,15 @@ device=/dev/sda1
 
 while sleep 1
 do
+	# filename=/media/sda1/$(date +%Y%m%d-%H%M%S.mp4)
 	filename=/media/sda1/$(date +%Y%m%d-%H%M%S.mp4)
 	mounted=$(mount|grep ${device})
 	if [ "" == "${mounted}" ]
 	then
 		echo ${device} not mounted yet
 	else
-		nc -w 2 localhost 56789 | ffmpeg -y -f h264 -r 30 -i - -an -codec copy ${filename}
+		# nc -w 2 localhost 56789 | ffmpeg -y -f h264 -r 30 -i - -an -codec copy ${filename}
+		nc -w 2 localhost 56789 | dd of=${filename} status=progress
 		size=$(du -hs ${filename}| cut -f 1)
 		if [ "${size}" == "0" ]
 		then
