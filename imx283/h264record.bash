3ef2e1d0682dd6300cea57b698d60dce0722c357
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-11-08 15:45:11 +0100

    Record directly to MP4 thanks to ffmpeg

---
+++ imx283/h264record.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+device=/dev/sda1
+
+while sleep 1
+do
+	filename=/media/sda1/$(date +%Y%m%d-%H%M%S.mp4)
+	mounted=$(mount|grep ${device})
+	if [ "" == "${mounted}" ]
+	then
+		echo ${device} not mounted yet
+	else
+		nc -w 2 localhost 56789 | ffmpeg -y -f h264 -r 30 -i - -an -codec copy ${filename}
+		size=$(du -hs ${filename}| cut -f 1)
+		if [ "${size}" == "0" ]
+		then
+			echo "${filename} is empty, removing"
+			rm -vf ${filename}
+		fi
+	fi
+	sleep 1
+done
+
