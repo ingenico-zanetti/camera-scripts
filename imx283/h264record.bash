83e0b94f04920ef3daeaa81aa4fa4bdf535055e8
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-11-09 01:07:53 +0100

    Revert "Shutdown sequence is not compatible with mp4 now, because ffmpeg doesn't"
    This reverts commit fd1bff0d510400494ee543fd4f48e86ed374a36c.

--- imx283/h264record.bash
+++ imx283/h264record.bash
@@ -4,15 +4,13 @@ device=/dev/sda1
 
 while sleep 1
 do
-	# filename=/media/sda1/$(date +%Y%m%d-%H%M%S.mp4)
 	filename=/media/sda1/$(date +%Y%m%d-%H%M%S.mp4)
 	mounted=$(mount|grep ${device})
 	if [ "" == "${mounted}" ]
 	then
 		echo ${device} not mounted yet
 	else
-		# nc -w 2 localhost 56789 | ffmpeg -y -f h264 -r 30 -i - -an -codec copy ${filename}
-		nc -w 2 localhost 56789 | dd of=${filename} status=progress
+		nc -w 2 localhost 56789 | ffmpeg -y -f h264 -r 30 -i - -an -codec copy ${filename}
 		size=$(du -hs ${filename}| cut -f 1)
 		if [ "${size}" == "0" ]
 		then
