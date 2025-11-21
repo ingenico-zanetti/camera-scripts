269d7ee327012abf6732f1de81e6389b3a3ad457
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-11-21 01:42:33 +0100

    Use configuration file instead of parameter for calling libcamera-vid

--- imx585/h264stream.bash
+++ imx585/h264stream.bash
@@ -2,84 +2,22 @@
 PATH="/usr/local/bin:$PATH"
 if [ "" = "$UHD" ]
 then
-	mode=" --mode 1928:1090:12 "
-	resolution=" --width 1920 --height 1080 "
-	DEFAULT_FRAME_RATE=60
+	echo "Binning 2x2"
+	configuration="UHD=no.cfg"
 	log="$(date +%Y%m%d-%H%M%S.UHD=no.log)"
-
 elif [ 1 -eq "$UHD" ]
 then
-	mode=" --mode 3856:2180:12 "
-	resolution=" --width 3840 --height 2160 "
-	DEFAULT_FRAME_RATE=15
+	echo "Actual 4K"
+	configuration="UHD=1.cfg"
 	log="$(date +%Y%m%d-%H%M%S.UHD=1.log)"
 else
-	mode=" --mode 3856:2180:12 "
-	resolution=" --width 1920 --height 1080 "
-	DEFAULT_FRAME_RATE=30
+	echo "1080p from 4K sensor"
+	configuration="UHD=0.cfg"
 	log="$(date +%Y%m%d-%H%M%S.UHD=0.log)"
 fi
-if [ "$D" = "" ]
-then
-	duration=" -t 0 "
-else
-	duration=" -t ${D}000 "
-fi
-
-if [ "" = "$G" ]
-then
-	gain=" --gain 1 "
-else
-	if [ "0" == "$G" ]
-	then
-		gain=""
-	else
-		gain=" --gain ${G} "
-	fi
-fi
-
-if [ "" = "$F" ]
-then
-	fps=" --framerate ${DEFAULT_FRAME_RATE} "
-else
-	fps=" --framerate ${F} "
-fi
-
-if [ "" = "$S" ]
-then
-	shutter=" --shutter 16000 "
-else
-	shutter=" --shutter ${S} "
-fi
-
-if [ "$B" = "" ]
-then
-	bitrate=" --bitrate 48000000 "
-else
-	bitrate=" --bitrate ${B} "
-fi
-
-wb="  --awbgains 1.64,2.05 "
-
-echo "Using ${duration} ; ${gain} ; ${fps} ; ${shutter} ; ${wb} ; ${mode} ; ${resolution} ; ${bitrate} and logging to ${log}"
 
 while sleep 1
 do
-	# echo "libcamera-vid --verbose --info-text \"frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg\"  ${mode}  ${shutter}  ${gain}  ${fps}  ${duration}  ${resolution}  --metering average --exposure sport  ${bitrate} --codec h264 -o - --libav-format h264 2>>${log} | ./h264streamer"
-
-	libcamera-vid --verbose --info-text "frame %frame (%fps fps) exp %exp ag %ag dg %dg rg %rg bg %bg" \
-		${mode} \
-		${shutter} \
-		${gain} \
-		${fps} \
-	       	${duration} \
-		${resolution} \
-		--metering average --exposure sport \
-		${wb} \
-		${bitrate} --codec h264 -o - --libav-format h264 2>>${log} | ./h264streamer 
-	if [ "$D" != "" ]
-	then
-		exit 0
-	fi
+	libcamera-vid --config ${configuration} 2>>${log} | ./h264streamer 
 done
 
