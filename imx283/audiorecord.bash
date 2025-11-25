c84b351cc8096694bbf0e9e16fedd719a1b9f0ec
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-11-25 09:50:56 +0100

    Add audio recording capability Only look for H2n as audio source.

---
+++ imx283/audiorecord.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+device=/dev/sda1
+
+while sleep 1
+do
+	filename=/media/sda1/$(date +%Y%m%d-%H%M%S.wav)
+	mounted=$(mount|grep ${device})
+	if [ "" == "${mounted}" ]
+	then
+		echo ${device} not mounted yet
+	else
+		ffmpeg -f alsa -i hw:CARD=H2n,DEV=0 ${filename}
+		size=$(du -hs ${filename}| cut -f 1)
+		if [ "${size}" == "0" ]
+		then
+			echo "${filename} is empty, removing"
+			rm -vf ${filename}
+		fi
+	fi
+	if [ -f /tmp/shutdown ]
+	then
+		break;
+	fi
+	sleep 1
+done
+
