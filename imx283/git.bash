4f2203fabf35465adfba75fcca72e30f2abe908d
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2025-12-04 13:21:04 +0100

    Add the basic configuration to allow commits

---
+++ imx283/git.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install -y vim
+git config --global user.email "arnaud.zanetti@free.fr"
+git config --global user.name "Arnaud ZANETTI"
+git config --global core.editor "vim"
+
